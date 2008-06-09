150d43c813994d81eea1d501e7f792fefb08c8fb
Dave Thompson <dthompson@tigris.org>
2008-06-09 11:57:59 +0000

    Merging linus' changes from rev 220, 221 (releases/VERSION_0_25_5) into /trunk.
    git-svn-id: http://argoumlinstaller.tigris.org/svn/argoumlinstaller/trunk@222 b9a5211d-681e-0410-b32f-aad809e617a8

--- build-release.sh
+++ build-release.sh
@@ -283,8 +283,10 @@ then
   fi
 
   # Build things
-  echo Building package in core
+  echo Build the core argouml that all projects depend on
   cd $DESTDIR/argouml && $RELATIVE_ANT install
+
+  echo Build all projects
   (
     cd $DESTDIR
     for proj in $PROJECTS
@@ -293,6 +295,9 @@ then
       ( cd $proj && ../argouml/$RELATIVE_ANT install )
     done
   )
+
+  echo Update the manifest to include modules files.
+  cd $DESTDIR/argouml && $RELATIVE_ANT update-argouml.jar-manifest
 fi
 
 if $builddoc
