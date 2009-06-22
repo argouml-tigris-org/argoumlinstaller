ae251f4d476b2ecbbdda8cd8064037ad3c35c13c
Linus Tolke <linus@tigris.org>
2009-06-22 04:59:17 +0000

    Refactor the script to simplify preparing for branches.
    git-svn-id: http://argoumlinstaller.tigris.org/svn/argoumlinstaller/trunk@299 b9a5211d-681e-0410-b32f-aad809e617a8

--- build-release.sh
+++ build-release.sh
@@ -32,6 +32,9 @@ PROJECTS=" \
     argouml-documentation \
     "
 
+# This is factored out to make it easier to release from branches.
+BRANCH_FROM=trunk
+
 RELATIVE_ANT=tools/apache-ant-1.7.0/bin/ant
 
 
@@ -106,7 +109,7 @@ verifyalldefaultidentitiessetup() {
   do
     echo -n .
     svn propget --username $USERNAME dummy:tag \
-      http://$proj.tigris.org/svn/$proj/trunk
+      http://$proj.tigris.org/svn/$proj/$BRANCH_FROM
   done
   echo done.
 }
@@ -143,7 +146,7 @@ then
   echo done.
 
   echo Checking for ant
-  if svn info http://argouml.tigris.org/svn/argouml/trunk/$RELATIVE_ANT 2>/dev/null |
+  if svn info http://argouml.tigris.org/svn/argouml/$BRANCH_FROM/$RELATIVE_ANT 2>/dev/null |
       grep -q "Node Kind: file"
   then
     : ok it does exist
@@ -162,7 +165,7 @@ then
     verifyalldefaultidentitiessetup and setting
 
     # Fixing subclipse:tags property for the main project
-    svn info http://argouml.tigris.org/svn/argouml/trunk |
+    svn info http://argouml.tigris.org/svn/argouml/$BRANCH_FROM |
     grep '^Revision:' |
     while read zz rev
     do
@@ -179,7 +182,7 @@ then
       do
         svn_add_prop subclipse:tags \
 	    "$rev,$VERSIONNAME,/releases/$VERSIONNAME/$subdir,tag" \
-	    http://argouml.tigris.org/svn/argouml/trunk/$subdir
+	    http://argouml.tigris.org/svn/argouml/$BRANCH_FROM/$subdir
       done
     done
 
@@ -187,14 +190,14 @@ then
     do
       echo Creating the release in $proj
 
-      svn info http://$proj.tigris.org/svn/$proj/trunk |
+      svn info http://$proj.tigris.org/svn/$proj/$BRANCH_FROM |
       grep '^Revision:' |
       while read zz rev
       do
-        svn_add_prop subclipse:tags "$rev,$VERSIONNAME,/releases/$VERSIONNAME,tag" http://$proj.tigris.org/svn/$proj/trunk
+        svn_add_prop subclipse:tags "$rev,$VERSIONNAME,/releases/$VERSIONNAME,tag" http://$proj.tigris.org/svn/$proj/$BRANCH_FROM
       done
 
-      svn copy --username $USERNAME http://$proj.tigris.org/svn/$proj/trunk http://$proj.tigris.org/svn/$proj/releases/$VERSIONNAME -m"Creating the release $RELEASE"
+      svn copy --username $USERNAME http://$proj.tigris.org/svn/$proj/$BRANCH_FROM http://$proj.tigris.org/svn/$proj/releases/$VERSIONNAME -m"Creating the release $RELEASE"
     done
   else
     exit 0
